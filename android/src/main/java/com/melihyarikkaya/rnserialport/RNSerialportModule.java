3036b9b6c5dd4a51dd94ac7fdd9b0bcbb8d3af8d
Simon Hancock <simon@linkingdrones.com>
2024-07-05 19:39:31 +0100

    Sent intent to be immutable

--- android/src/main/java/com/melihyarikkaya/rnserialport/RNSerialportModule.java
+++ android/src/main/java/com/melihyarikkaya/rnserialport/RNSerialportModule.java
@@ -739,7 +739,7 @@ public class RNSerialportModule extends ReactContextBaseJavaModule implements Li
   private void requestUserPermission(UsbDevice device) {
     if(device == null)
       return;
-    PendingIntent mPendingIntent = PendingIntent.getBroadcast(mReactContext, 0 , new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_MUTABLE);
+    PendingIntent mPendingIntent = PendingIntent.getBroadcast(mReactContext, 0 , new Intent(ACTION_USB_PERMISSION), PendingIntent.FLAG_IMMUTABLE);
     usbManager.requestPermission(device, mPendingIntent);
   }
 
